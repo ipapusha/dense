d4890b2a4d7f4ba7e4057cd237b79a5db257ce87
Ivan Papusha <ivan.papusha@gmail.com>
2015-01-28 17:58:52 -0800

    grammar potentially good

--- src/parse.h
+++ src/parse.h
@@ -4,54 +4,120 @@
 #include "cs.h"
 
 /* interface to lexer */
-extern int yylineno;		/* from lexer */
+extern int yylineno				/* from lexer */
 void yyerror(char *s, ...);
 
-/* AST node structures */
-enum nodetype {
-	nPLUS, nMINUS, nTIMES, nDOTTIMES, nDIV, nDOTDIV,
-	nHLIST, nVLIST,
-	nSCALARVAL,
-	nMATRIXVAL,
-	nSYMREF,
-	nSYMASGN
+/* grammar */
+struct prog {
+	struct stmt_list *sl;
 };
-struct symbol {
-	char *name;				/* a variable name */
-	struct ast *a;			/* value of symbol */
+struct stmt_list {
+	struct stmt *s;
+	struct stmt_list *next;
 };
-struct astpair {
-	struct ast *l;			/* left branch */
-	struct ast *r;			/* right branch */ 
-}; 
-struct astlist {
-	struct ast *a;			/* linked list */
-	struct astlist *next;
-};
-struct ast {
-	enum nodetype type;
+typedef enum { sASSIGN, sEXPR } stmt_e;
+struct stmt {
+	stmt_e type;
 	union {
-		double sv;			/* scalar value */
-		cs *mv;				/* matrix value */
-		struct symbol *s;	/* symbol ref */
-		struct astpair *ap;	/* contains: l and r */
-		struct astlist *al;	/* contains: a and next */
+		struct assign_stmt *as;
+		struct expr_stmt *es;
+	} op;
+};
+struct assign_stmt {
+	struct ident *id;
+	struct expr *e;
+};
+struct expr_stmt {
+	struct expr *e;
+};
+struct expr_list {				/* expr separated by commas */
+	struct expr *e;
+	struct expr_list *next;
+};
+struct concat_list {			/* expr lists separated by semicolons */
+	struct expr_list *el;
+	struct concat_list *next;
+};
+typedef enum { idVAR, idFUNC } ident_e;
+struct ident {					/* identifier */
+	ident_e type;				/* TODO: add idPARAMVAR */
+	char *name;
+	struct expr_list *el;		/* optional arguments list */
+};
+typedef enum { mCL, mEL, mMV } matrix_e;
+struct matrix {					/* abstract matrix type */
+	matrix_e type;
+	union {
+		struct concat_list *cl;	/* vertical concatenation */
+		struct expr_list *el;	/* horizontal concatenation */
+		cs *mv;					/* resolved matrix value */
+	} op;
+};
+typedef enum { e2BACKSLASH, e2DIV, e2DOTDIV, e2DOTEXP, e2DOTMUL, e2EXP,
+				e2MINUS, e2MUL, e2PLUS } expr2_e;
+struct expr2 {					/* binary expression */
+	expr2_e type;
+	struct expr *l;
+	struct expr *r;
+};
+typedef enum { e1MINUS, e1PLUS } expr1_e;
+struct expr1 {					/* unary expression */
+	expr1_e type;
+	struct expr *e;
+};
+
+typedef enum { eIDENT, eNUMBER, eMATRIX, eEXPR2, eEXPR1, eTRANSPOSE, eFUNCALL }
+				expr_e;
+struct expr {					/* general expression */
+	expr_e type;
+	union {
+		struct ident *id;		/* identifier */
+		double sv;				/* scalar number value */
+		struct matrix *m;		/* abstract matrix */
+		struct expr2 *e2;		/* binary expression */
+		struct expr1 *e1;		/* unary expression */
+		struct expr *e;			/* expr to transpose */
 	} op;
 };
 
 /* build an AST */
-struct ast *newpair(enum nodetype type, struct ast *l, struct ast *r);
-struct ast *newlist(enum nodetype type, struct astlist *al);
-struct ast *newscalarval(double sv);
-struct ast *newmatrixval(cs *mv);
-struct ast *newsymref(struct symbol *s);
-struct ast *newsymasgn(struct symbol *s, struct ast *v);
+struct prog *newprog (struct stmt_list *sl);
+struct stmt_list *newstmt_list (struct stmt *s, struct stmt_list *next);
+struct stmt *newstmt (stmt_e type, struct assign_stmt *as, 
+		struct expr_stmt *es);
+struct assign_stmt *newassign_stmt (struct ident *id, struct expr *e);
+struct expr_stmt *newexpr_stmt (struct expr *e);
+struct expr_list *newexpr_list (struct expr *e, struct expr_list *next);
+struct concat_list *newconcat_list (struct expr_list *el, 
+		struct concat_list *next);
+struct ident *newident (ident_e type, char *name, struct expr_list *el);
+struct matrix *newmatrix (matrix_e type, struct concat_list *cl, 
+		struct expr_list *el, cs *mv);
+struct expr2 *newexpr2 (expr2_e type, struct expr *l, struct expr *r);
+struct expr1 *newexpr1 (expr1_e type, struct expr *e);
+struct expr *newexpr (expr_e type, struct ident *id, double sv, 
+		struct matrix *m, struct expr2 *e2, struct expr1 *e1, struct expr *e);
+
+/* clean up AST */
+int delprog(struct prog *);
+int delstmt_list(struct stmt_list *);
+int delstmt(struct stmt *);
+int delassign_stmt(struct assign_stmt *);
+int delexpr_stmt(struct expr_stmt *);
+int delexpr_list(struct expr_list *);
+int delconcat_list(struct concat_list *);
+int delident(struct ident *);
+int delmatrix(struct matrix *);
+int delexpr2(struct expr2 *);
+int delexpr1(struct expr1 *);
+int delexpr(struct expr *);
 
 /* simple symtab of fixed size */
 #define M13 8191
 #ifndef NHASH
 #define NHASH M13
 #endif
+/*
 struct symtablist {
 	struct symbol *s;
 	struct symtablist *next;
@@ -60,9 +126,6 @@ struct symtablist symtab[NHASH];
 struct symbol *symlookup(char *name);
 void symadd(struct symbol *s);
 void symdel(struct symbol *s);
-
-/* evaluate and clean up an AST */
-void eval(struct ast *);
-void treefree(struct ast *);
+*/
 
 #endif
--- src/parse.y
+++ src/parse.y
@@ -1,79 +1,103 @@
 %{
-#include <stdio.h>
-#include <stdlib.h>
-#include "parse.h"
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include "parse.h"
 %}
 
 %union {
-	struct ast *a;
-	struct symbol *s;	/* which symbol */
+	struct prog			*pr;
+	struct stmt_list	*sl;
+	struct stmt			*s;
+	struct assign_stmt	*as;
+	struct expr_stmt	*es;
+	struct expr_list	*el;
+	struct concat_list	*cl;
+	struct ident		*id;
+	struct matrix		*m;
+	struct expr2		*e2;
+	struct expr1		*e1;
+	struct expr			*e;
+	char *str;
 	double d;
-	int fn;
 }
 
-/* declare tokens */
-%token <d> NUMBER
-%token <s> NAME
-%token <fn> FUNC
-%token EOL
+%token <str> IDENT 
+%token <d> NUMBER 
 %token LBRACKET RBRACKET
+%token SEMI
 
-/* precedence */
 %right EQ
-%left SEMICOLON
 %left COMMA
 %left PLUS MINUS
-%left MUL DIV DOTMUL DOTDIV
+%left MUL DIV DOTMUL DOTDIV BACKSLASH
 %right UMINUS
 %right TRANSPOSE
 %right EXP DOTEXP
 %nonassoc LPAREN RPAREN
 
-%start stmtlist
+%type <pr> prog
+%type <sl> stmt_list
+%type <s> stmt
+%type <as> assign_stmt
+%type <es> expr_stmt
+%type <el> expr_list
+%type <cl> concat_list
+%type <id> ident
+%type <m> matrix
+%type <e2> expr2
+%type <e1> expr1
+%type <e> expr
 
-/*
- %nonassoc <fn> CMP
- %right '='
- %left '+' '-'
- %left '*' '/'
- %nonassoc '|' UMINUS
- 
- %type <a> exp stmt list explist
- %type <sl> symlist
- 
- %start calclist
-*/
+%% /* grammar rules */
 
-%%
-
-/* anything that evaluates to a matrix */
-exp: exp PLUS exp				{}
-	| exp MINUS exp				{}
-	| exp MUL exp				{}
-	| exp DIV exp				{}
-	| exp DOTMUL exp			{}
-	| exp DOTDIV exp			{}
-	| MINUS exp %prec UMINUS	{}
-	| LPAREN exp RPAREN			{}
-	| NUMBER					{} /* returns scalar val, compare to vlist */
-	| NAME						{}
-	| NAME EQ exp				{}
-	| FUNC LPAREN hlist RPAREN	{} /* built-in function call, TODO: make explist by itself */
+prog: stmt_list 
 	;
-
-hlist: exp
-	| exp COMMA hlist
-	| LBRACKET hlist RBRACKET
+stmt_list: stmt
+		 | stmt_list stmt
+		 ;
+stmt: assign_stmt
+	| expr_stmt
 	;
-
-vlist: hlist
-	| hlist SEMICOLON vlist
-	| LBRACKET hlist SEMICOLON vlist RBRACKET
-	;
-
-exp: vlist	 /* returns matrix val, compare to NUMBER */
-	;
-
-stmtlist: /* nothing */
-	| exp SEMICOLON stmtlist
-	;
+assign_stmt: ident EQ expr SEMI
+		   ;
+expr_stmt: expr SEMI
+		 ;
+/* expr and concat lists */
+expr_list : expr
+		  | expr_list COMMA expr
+		  ;
+concat_list : expr_list SEMI expr_list
+            | concat_list SEMI expr_list
+			;
+/* EXPR */
+expr : ident
+     | NUMBER
+     | matrix
+     | expr2	
+     | expr1
+	 | LPAREN expr RPAREN
+	 | expr TRANSPOSE
+	 | ident LPAREN expr_list RPAREN 
+     | ident LPAREN RPAREN
+	 ;
+ident: IDENT
+	 ;
+matrix : LBRACKET RBRACKET
+       | LBRACKET concat_list RBRACKET
+       | LBRACKET concat_list SEMI RBRACKET
+       | LBRACKET expr_list RBRACKET
+       | LBRACKET expr_list SEMI RBRACKET
+	   ;
+expr2 : expr BACKSLASH expr
+      | expr DIV expr
+      | expr DOTDIV expr
+      | expr DOTEXP expr
+      | expr DOTMUL expr
+      | expr EXP expr
+      | expr MINUS expr
+      | expr MUL expr
+      | expr PLUS expr
+	  ;
+expr1 : MINUS expr %prec UMINUS
+      | PLUS expr %prec UMINUS
+	  ;
